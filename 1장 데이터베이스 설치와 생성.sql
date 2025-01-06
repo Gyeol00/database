965708318b1a66fc22518cb59e1fb0bf6089807c
gyeol000726 <gyeol000726@gmail.com>
2025-01-06 17:37:15 +0900

    1장 데이터베이스 실습

---
+++ 1장 데이터베이스 설치와 생성.sql
@@ -0,0 +1,17 @@
+#날짜 : 2025/01/06
+#이름 : 한결
+#내용 : 1장 데이터베이스 설치와 생성
+
+#실습 1-1
+create database `StudyDB`;
+drop database `StudyDB`;
+
+#실습 1-2
+create user 'gyeol00'@'%' identified by '1234';
+grant all privilegeson StudyDB.* to 'gyeol00'@'%';
+flush privileges;
+
+#실습 1-3
+alter user 'gyeol00'@'%' identified by 'abc1234';
+drop user 'gyeol00'@'%';
+flish privi
